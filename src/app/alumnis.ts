aef62320caadfc734c878fe75a7ba078984985ab
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-07-03 12:23:13 +0200

    update alumni.ts

--- src/app/alumnis.ts
+++ src/app/alumnis.ts
@@ -1,33 +1,33 @@
-export const dishes = [
+export const alumnis = [
     {
         name: 'Hesham Ahmed',
         photo: 'assets/alumni/photo.jpg',
         career: 'Full Stack Developer',
-        achievements: ''
+        achievements: '100%'
     }, {
         name: 'Mara Federmair',
         photo: 'assets/alumni/photo.jpg',
         career: 'Full Stack Developer',
-        achievements: ''
+        achievements: '100%'
     }, {
         name: 'Junufa Simonis',
         photo: 'assets/alumni/photo.jpg',
         career: 'Full Stack Developer',
-        achievements: ''
+        achievements: '100%'
     }, {
         name: 'Mehdi Salimi',
         photo: 'assets/alumni/photo.jpg',
         career: 'Full Stack Developer',
-        achievements: ''
+        achievements: '100%'
     }, {
         name: 'Emelin Bilajbeovic',
         photo: 'assets/alumni/photo.jpg',
         career: 'Full Stack Developer',
-        achievements: ''
+        achievements: '100%'
     }, {
         name: 'Gazmend Aliu',
         photo: 'assets/alumni/photo.jpg',
         career: 'Full Stack Developer',
-        achievements: ''
+        achievements: '100%'
     },
 ]
