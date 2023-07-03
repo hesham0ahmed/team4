bfc510db8f68b1402d8fd4a2159fc964e4aaa1ef
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-07-03 14:50:32 +0200

    added new photos and scss for cards

--- src/app/alumnis.ts
+++ src/app/alumnis.ts
@@ -1,7 +1,7 @@
 export const alumnis = [
     {
         name: 'Hesham Ahmed',
-        photo: 'assets/alumni/photo.jpg',
+        photo: 'assets/alumni/man.png',
         career: 'Full Stack Developer',
         achievements: '100%'
     }, {
@@ -16,17 +16,17 @@ export const alumnis = [
         achievements: '100%'
     }, {
         name: 'Mehdi Salimi',
-        photo: 'assets/alumni/photo.jpg',
+        photo: 'assets/alumni/man.png',
         career: 'Full Stack Developer',
         achievements: '100%'
     }, {
         name: 'Emelin Bilajbegovic',
-        photo: 'assets/alumni/photo.jpg',
+        photo: 'assets/alumni/man.png',
         career: 'Full Stack Developer',
         achievements: '100%'
     }, {
         name: 'Gazmend Aliu',
-        photo: 'assets/alumni/photo.jpg',
+        photo: 'assets/alumni/man.png',
         career: 'Full Stack Developer',
         achievements: '100%'
     },
