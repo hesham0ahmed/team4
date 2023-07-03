79a619b2841b95400e11edf091e6b44f8a72ca59
Mara Federmair <mara@klingt.org>
2023-07-03 13:16:22 +0200

    routes-membershipComponents-mara

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,11 @@ import { AchievementsComponent } from './achievements/achievements.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeroComponent } from './hero/hero.component';
 import { EventsComponent } from './events/events.component';
+import { MembershipComponent } from './membership/membership.component';
+import { FormsOfMembershipComponent } from './forms-of-membership/forms-of-membership.component';
+import { GetActiveComponent } from './get-active/get-active.component';
+import { MentoringComponent } from './mentoring/mentoring.component';
+import { BenefitsComponent } from './benefits/benefits.component';
 
 
 const routes: Routes = [
@@ -16,7 +21,23 @@ const routes: Routes = [
   },
   {
     path: "events", component: EventsComponent
+  },
+  {
+    path: "membership", component: MembershipComponent
+  },
+  {
+    path: "forms-of-membership", component: FormsOfMembershipComponent
+  },
+  {
+    path: "get-active", component: GetActiveComponent
+  },
+  {
+    path: "mentoring", component: MentoringComponent
+  },
+  {
+    path: "benefits", component: BenefitsComponent
   }
+
 
 
 
