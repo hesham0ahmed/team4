755cdd24d5450eb68b1371352378cf8e816093b5
Mehdi Salimi <salimimehdi@yahoo.com>
2023-07-05 23:15:11 +0200

    event-merge

--- src/app/merge/merge.component.ts
+++ src/app/merge/merge.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Merge } from '../Merge';
 import { IMerge } from '../IMerge';
 import {ActivatedRoute, Router} from '@angular/router';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-merge',
@@ -11,20 +13,39 @@ import {ActivatedRoute, Router} from '@angular/router';
 export class MergeComponent implements OnInit{
   events_arr : IMerge[] = [];
   id_detail : number= -1;
-  private mywindow : any;
-  photo : string ="";
+
+  
 
   constructor(private route : ActivatedRoute , private router : Router) {
     
   }
 
-  show(id : number) {
-    if(this.mywindow) {
-      this.mywindow.close();
-     }
-     //console.log(URL);
-     this.photo = "/assets/images/"+this.events_arr[id].photo;
-     this.mywindow = window.open(this.photo, "_blank", "resizable=no, titlebar=0, toolbar=no, scrollbars=no, menubar=no, status=no, directories=no, location=no, width=800, height=600, left=300, top=100 " );
+  show(item : IMerge) {
+
+    Swal.fire ({ 
+      
+      imageUrl : `/assets/images/${item.photo}` ,
+      position : 'center' ,
+      html : `<div class="d-flex align-items-center justify-content-center">
+      <div class="card" style="width: 22rem;">
+      ${(item.available == false) ? "<div style='color :red;'>Not Available!</div>" : ""}
+      <div class="card-body">
+        <h5 class="card-title">${item.name}</h5>
+          <h5 class="card-title">normal price : ${item.price}$</h5>
+          <h5 class="card-title">member price : ${item.xprice}$</h5>
+          <p class="card-text" >
+              ${item.description}
+          </p>
+      </div>
+      </div>
+      </div>` ,
+      confirmButtonText : 'Close' ,
+      confirmButtonColor : '#3B71CA' ,
+      imageWidth : 'auto' ,
+      imageHeight : 'auto' ,
+
+    })
+    
   }
 
   ngOnInit(): void {
