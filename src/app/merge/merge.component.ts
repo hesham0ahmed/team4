d771121a76b304ce2c781925c30148f08483ddc2
Mehdi Salimi <salimimehdi@yahoo.com>
2023-07-05 12:20:04 +0200

    changes

--- src/app/merge/merge.component.ts
+++ src/app/merge/merge.component.ts
@@ -11,13 +11,21 @@ import {ActivatedRoute, Router} from '@angular/router';
 export class MergeComponent implements OnInit{
   events_arr : IMerge[] = [];
   id_detail : number= -1;
-
-  
+  private mywindow : any;
+  photo : string ="";
 
   constructor(private route : ActivatedRoute , private router : Router) {
     
   }
 
+  show(id : number) {
+    if(this.mywindow) {
+      this.mywindow.close();
+     }
+     //console.log(URL);
+     this.photo = "/assets/images/"+this.events_arr[id].photo;
+     this.mywindow = window.open(this.photo, "_blank", "resizable=no, titlebar=0, toolbar=no, scrollbars=no, menubar=no, status=no, directories=no, location=no, width=800, height=600, left=300, top=100 " );
+  }
 
   ngOnInit(): void {
 
