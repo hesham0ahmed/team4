8e2493bb73d81e9066d9c67a88d37834d4b1ddb5
JanA867 <jsc.simonis@gmail.com>
2023-07-06 12:44:27 +0200

    file

--- src/app/career/career.component.ts
+++ src/app/career/career.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-career',
   templateUrl: './career.component.html',
-  styleUrls: ['./career.component.scss']
+  styleUrls: ['./career.component.scss'],
 })
 export class CareerComponent {
+  // pdfSrc = 'assets/alumni/CV-Template.pdf';
 
+  downloadPDF() {
+    let link = document.createElement('a');
+    link.download = 'CV-Template.pdf';
+    link.href = 'assets/CV-Template.pdf';
+    link.click();
+  }
 }
